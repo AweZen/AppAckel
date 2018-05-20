67727b5e691370ae3fad00dceb616c5d5156166e
AweZen <35920102+AweZen@users.noreply.github.com>
2018-05-20 14:08:53 +0200

    Databas
    Börjat bygga upp en lokaldatabas. Behövs göras en index.php, tror att dbOperations fungerar. Om någon vill fortsätta på jobbet finns https://www.simplifiedcoding.net/volley-with-kotlin/

---
+++ app/src/main/java/com/example/test/test/EndPoints.kt
@@ -0,0 +1,6 @@
+package com.example.test.test
+
+object EndPoints {
+    private val URL_ROOT = "localhost/app/"
+    val URL_ADD_BID = URL_ROOT + "insert.php"
+}
--- app/src/main/java/com/example/test/test/MainActivity.kt
+++ app/src/main/java/com/example/test/test/MainActivity.kt
@@ -7,9 +7,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.content.Intent
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,21 +26,62 @@ class MainActivity : AppCompatActivity() {
         val mailET = (R.id.editText12)as EditText
         val addresET = findViewById(R.id.editText13)as EditText
         val bidET = findViewById(R.id.editText14)as EditText*/
+        val userName= findViewById(R.id.editText10)as EditText
+        val userPhone=findViewById(R.id.editText11)as EditText
+        val userMail=findViewById(R.id.editText12)as EditText
+        val userAddres=findViewById(R.id.editText13)as EditText
+        val userBid=findViewById(R.id.editText14)as EditText
         val bidButton = findViewById(R.id.button)as Button
         bidButton.setOnClickListener() {
             Toast.makeText(this@MainActivity, "Bud skickat", Toast.LENGTH_SHORT).show()
-            /*val userName=nameET.text
-            val userPhone=phoneET.text
-            val userMail=mailET.text
-            val userAddres=addresET.text
-            val userBid=bidET.text*/
-            //skicka alla variabler till databasen
+            /*      val userName= findViewById(R.id.editText10)as EditText
+            val userPhone=findViewById(R.id.editText11)as EditText
+            val userMail=findViewById(R.id.editText12)as EditText
+            val userAddres=findViewById(R.id.editText13)as EditText
+            val userBid=findViewById(R.id.editText14)as EditText    */
+        }
+        fun addArtist() {
+            //getting the record values
+            val name = userName?.text.toString()
+            val phone = userPhone?.text.toString()
+            val mail = userMail?.text.toString()
+            val adress = userAddres?.text.toString()
+            val bid = userBid?.text.toString()
+
+            //creating volley string request
+            val stringRequest = object : StringRequest(Request.Method.POST, EndPoints.URL_ADD_BID,
+                    Response.Listener<String> { response ->
+                        try {
+                            val obj = JSONObject(response)
+                            Toast.makeText(applicationContext, obj.getString("message"), Toast.LENGTH_LONG).show()
+                        } catch (e: JSONException) {
+                            e.printStackTrace()
+                        }
+                    },
+                    object : Response.ErrorListener {
+                        override fun onErrorResponse(volleyError: VolleyError) {
+                            Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show()
+                        }
+                    }) {
+                @Throws(AuthFailureError::class)
+                override fun getParams(): Map<String, String> {
+                    val params = HashMap<String, String>()
+                    params.put("Namn", name)
+                    params.put("Telefon", phone)
+                    params.put("Mail", mail)
+                    params.put("Adress", adress)
+                    params.put("Bud", bid)
+                    return params
+                }
+            }
+
+            //adding request to queue
+            VolleySingleton.instance?.addToRequestQueue(stringRequest)
         }
 
 
 
     }
-
 
 
 
