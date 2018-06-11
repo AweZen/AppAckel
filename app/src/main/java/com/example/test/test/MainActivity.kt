e5e79e7d2f7a05c56840fbe6d3b2f4cc7a72810f
Oscar Lang <oscar.lang98@gmail.com>
2018-06-11 20:14:58 +0200

    Fixade några iconer till actionbaren

--- app/src/main/java/com/example/test/test/MainActivity.kt
+++ app/src/main/java/com/example/test/test/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
 import android.content.Intent
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
@@ -17,71 +19,64 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ET=editText
-        /*val nameET = findViewById(R.id.editText10)as EditText
-        val phoneET = findViewById(R.id.editText11)as EditText
-        val mailET = (R.id.editText12)as EditText
-        val addresET = findViewById(R.id.editText13)as EditText
-        val bidET = findViewById(R.id.editText14)as EditText*/
-        val userName= findViewById(R.id.editText10)as EditText
-        val userPhone=findViewById(R.id.editText11)as EditText
-        val userMail=findViewById(R.id.editText12)as EditText
-        val userAddres=findViewById(R.id.editText13)as EditText
-        val userBid=findViewById(R.id.editText14)as EditText
-        val bidButton = findViewById(R.id.button)as Button
+        val bidButton = findViewById(R.id.button) as Button
         bidButton.setOnClickListener() {
             Toast.makeText(this@MainActivity, "Bud skickat", Toast.LENGTH_SHORT).show()
-            /*      val userName= findViewById(R.id.editText10)as EditText
-            val userPhone=findViewById(R.id.editText11)as EditText
-            val userMail=findViewById(R.id.editText12)as EditText
-            val userAddres=findViewById(R.id.editText13)as EditText
-            val userBid=findViewById(R.id.editText14)as EditText    */
+            addArtist()
         }
-        fun addArtist() {
-            //getting the record values
-            val name = userName?.text.toString()
-            val phone = userPhone?.text.toString()
-            val mail = userMail?.text.toString()
-            val adress = userAddres?.text.toString()
-            val bid = userBid?.text.toString()
-
-            //creating volley string request
-            val stringRequest = object : StringRequest(Request.Method.POST, EndPoints.URL_ADD_BID,
-                    Response.Listener<String> { response ->
-                        try {
-                            val obj = JSONObject(response)
-                            Toast.makeText(applicationContext, obj.getString("message"), Toast.LENGTH_LONG).show()
-                        } catch (e: JSONException) {
-                            e.printStackTrace()
-                        }
-                    },
-                    object : Response.ErrorListener {
-                        override fun onErrorResponse(volleyError: VolleyError) {
-                            Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show()
-                        }
-                    }) {
-                @Throws(AuthFailureError::class)
-                override fun getParams(): Map<String, String> {
-                    val params = HashMap<String, String>()
-                    params.put("Namn", name)
-                    params.put("Telefon", phone)
-                    params.put("Mail", mail)
-                    params.put("Adress", adress)
-                    params.put("Bud", bid)
-                    return params
-                }
-            }
-
-            //adding request to queue
-            VolleySingleton.instance?.addToRequestQueue(stringRequest)
-        }
-
-
-
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater= menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    fun addArtist() {
+        //getting the record values
+
+        val name = findViewById<EditText>(R.id.editText10).text.toString()
+        val phone = findViewById<EditText>(R.id.editText11).text.toString()
+        val mail = findViewById<EditText>(R.id.editText12).text.toString()
+        val adress = findViewById<EditText>(R.id.editText13).text.toString()
+        val bid = findViewById<EditText>(R.id.editText14).text.toString()
+
+        //creating volley string request
+        val stringRequest = object : StringRequest(Request.Method.POST, EndPoints.URL_ADD_BID,
+                Response.Listener<String> { response ->
+                    try {
+                        val obj = JSONObject(response)
+                        Toast.makeText(applicationContext, obj.getString("message"), Toast.LENGTH_LONG).show()
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                object : Response.ErrorListener {
+                    override fun onErrorResponse(volleyError: VolleyError) {
+                        Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show()
+                    }
+                }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params.put("Namn", name)
+                params.put("Telefon", phone)
+                params.put("Mail", mail)
+                params.put("Adress", adress)
+                params.put("Bud", bid)
+                return params
+            }
+        }
+
+        //adding request to queue
+        VolleySingleton.instance?.addToRequestQueue(stringRequest)
+    }
+
+
+
+
 
 
 
